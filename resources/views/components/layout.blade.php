63f5f0ce2ece6e135509f9c42135b88b39413fb5
gla-elte <gla@inf.elte.hu>
2023-04-26 22:00:10 +0200

    layout scss -> css

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blade névtelen komponensek bemutatása Tailwind CSS keretrendszerrel kiegészítve</title>
-    @vite('resources/sass/main.scss', 'resources/css/app.css')
+    {{-- @vite('resources/sass/main.scss', 'resources/css/app.css') --}}
+    @vite('resources/css/main.css', 'resources/css/app.css')
 </head>
 <body class="is-preload">
     {{-- {{ $slot }} --}}
