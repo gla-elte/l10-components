2e0de04483628a31425561f08b0fba38f41c3205
gla-elte <gla@inf.elte.hu>
2023-07-06 18:35:43 +0200

    Tag: n-n, edit, FIXED typo error

--- resources/views/tags/edit.blade.php
+++ resources/views/tags/edit.blade.php
@@ -9,7 +9,7 @@
     <label for="posts">Blogbejegyzései:</label>
     <select name="posts[]" id="posts" multiple>
       @forelse ($posts as $post)
-        <option value="{{ $post->id }} @selected($tag->posts->pluck('id')->contains($post->id))">{{ $post->title }}</option>
+        <option value="{{ $post->id }}" @selected($tag->posts->pluck('id')->contains($post->id)) >{{ $post->title }}</option>
       @empty
         <option value="0">Nincs még olyan blogbejegyzés, ami ehhez a címkéhez tartozna a blogon</option>
       @endforelse
